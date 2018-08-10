118ddbe3c3f6d7a44104397ec3edff066e8ba74e
Tawe Nganga <tawenga@yandex.com>
2018-08-11 01:09:43 +0300

    Clean enter phone layout

--- app/src/main/java/com/kelis/EnterPhoneActivity.java
+++ app/src/main/java/com/kelis/EnterPhoneActivity.java
@@ -3,6 +3,7 @@ package com.kelis;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -32,6 +33,9 @@ public class EnterPhoneActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_enter_phone);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         mPhoneNumberEditText = (EditText) findViewById(R.id.phone_number_edit_text);
 
